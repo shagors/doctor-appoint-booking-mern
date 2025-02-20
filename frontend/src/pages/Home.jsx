75fcd76527ea23e5309ed8935800dbc22d2e1277
shagors <shajjadshagor@gmail.com>
2025-02-20 17:31:17 +0600

    make top doctors section and design it

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import SpecialtyMenu from "../components/SpecialtyMenu";
+import TopDoctors from "../components/TopDoctors";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialtyMenu />
+      <TopDoctors />
     </div>
   );
 };
